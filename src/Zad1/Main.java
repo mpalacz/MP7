6ac61a086666e2e4eee766a667f3c5cffec5f460
student <mpalacz00@gmail.com>
2021-11-26 10:37:23 +0100

    Zad1 final

--- src/Zad1/Main.java
+++ src/Zad1/Main.java
@@ -1,12 +1,12 @@
 package Zad1;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args){
-        int[] array = {10,9,8,7,6,5,4,3,2,1};
+        int[] array = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
 
-        System.out.println("Przed pętlą:");
-        for (int i = 0; i < array.length; i++)
-            System.out.print(array[i] + " ");
+        System.out.println("Przed pętlą:\n" + Arrays.toString(array));
 
         for (int i = 0; i < array.length - 1; i++){
             if(array[i] > array[i + 1]) {
@@ -17,8 +17,6 @@ public class Main {
         }
 
         System.out.println();
-        System.out.println("Po przejściu pętli:");
-        for (int i = 0; i < array.length; i++)
-            System.out.print(array[i] + " ");
+        System.out.println("Po przejściu pętli:\n" + Arrays.toString(array));
     }
 }
