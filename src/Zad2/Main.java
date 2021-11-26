8b5d344e4431232a7066ca7d4a007084217455e6
mpala <mpalacz00@gmail.com>
2021-11-26 20:27:00 +0100

    Zad2c

--- src/Zad2/Main.java
+++ src/Zad2/Main.java
@@ -8,23 +8,33 @@ public class Main {
         System.out.println("Tablica początkowa:\n" + Arrays.toString(array));
 
         int licznik = 0;
-        //boolean czySzukac = true;
+        boolean czySzukac = true;
 
-        for (int j = 0; j < array.length /*&& czySzukac*/; j++) {
+        for (int j = 0; j < array.length && czySzukac; j++) {
             licznik++;
-            //czySzukac = false;
+            czySzukac = false;
             int min = j;
-            for (int i = j + 1; i < array.length; i++) {
+            int max = array.length - 1 - j;
+            for (int i = j; i < array.length - j; i++) {
                 licznik++;
                 if (array[i] < array[min]) {
                     min = i;
-                    //czySzukac = true;
+                    czySzukac = true;
+                    licznik++;
+                } else if (array[i] > array[max]){
+                    max = i;
+                    czySzukac = true;
                     licznik++;
                 }
             }
             int x = array[min];
+            if (j == max) max = min;
             array[min] = array[j];
             array[j] = x;
+
+            x = array[max];
+            array[max] = array[array.length - 1 - j];
+            array[array.length - 1 - j] = x;
         }
         System.out.println(Arrays.toString(array));
         System.out.println(licznik);
