c4eb5d3375808c93aaeee35a6048fa15ffd1fbcc
student <mpalacz00@gmail.com>
2021-11-26 11:42:18 +0100

    Zad1d

--- src/Zad1/Main.java
+++ src/Zad1/Main.java
@@ -4,22 +4,27 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args){
-        int[] array = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
-
+        //int[] array = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         System.out.println("Przed pętlą:\n" + Arrays.toString(array));
 
         int licznik = 0;
+
         for(int j = 0; j < array.length - 1; j++) {
             licznik++;
+            boolean czyPosortowana = true;
             for (int i = 0; i < array.length - 1 - j; i++) {
                 if (array[i] > array[i + 1]) {
                     int x = array[i];
                     array[i] = array[i + 1];
                     array[i + 1] = x;
                     licznik++;
+                    czyPosortowana = false;
                 }
                 licznik++;
             }
+            if(czyPosortowana)
+                break;
         }
 
         System.out.println();
